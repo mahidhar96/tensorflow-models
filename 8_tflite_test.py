23aa5815e7a620134cf60ecd93f1806b5951b0c6
Mahidhar <mahidhar96@gmail.com>
2020-03-11 14:48:46 +0530

    added file to test tflite

---
+++ 8_tflite_test.py
@@ -0,0 +1,25 @@
+#Use this link to visualize the network
+#https://lutzroeder.github.io/netron/
+
+import numpy as np
+import tensorflow as tf
+
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="detect_no_inference_type.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Test model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.uint8)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
