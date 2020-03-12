863fed4183dc39431cd633879ade71d5dfc5772e
anupam <anupamb266@gmail.com>
2020-03-12 13:23:20 +0530

    changes to remove warnings for v1_15

--- slim/datasets/build_visualwakewords_data.py
+++ slim/datasets/build_visualwakewords_data.py
@@ -96,51 +96,51 @@ tf.flags.DEFINE_string(
 
 FLAGS = flags.FLAGS
 
-tf.logging.set_verbosity(tf.logging.INFO)
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
 
 
 def main(unused_argv):
-  # Path to COCO dataset images and annotations
-  assert FLAGS.train_image_dir, '`train_image_dir` missing.'
-  assert FLAGS.val_image_dir, '`val_image_dir` missing.'
-  assert FLAGS.train_annotations_file, '`train_annotations_file` missing.'
-  assert FLAGS.val_annotations_file, '`val_annotations_file` missing.'
-  visualwakewords_annotations_train = os.path.join(
-      FLAGS.output_dir, 'instances_visualwakewords_train2014.json')
-  visualwakewords_annotations_val = os.path.join(
-      FLAGS.output_dir, 'instances_visualwakewords_val2014.json')
-  visualwakewords_labels_filename = os.path.join(FLAGS.output_dir,
-                                                 'labels.txt')
-  small_object_area_threshold = FLAGS.small_object_area_threshold
-  foreground_class_of_interest = FLAGS.foreground_class_of_interest
-  # Create the Visual WakeWords annotations from COCO annotations
-  if not tf.gfile.IsDirectory(FLAGS.output_dir):
-    tf.gfile.MakeDirs(FLAGS.output_dir)
-  build_visualwakewords_data_lib.create_visual_wakeword_annotations(
-      FLAGS.train_annotations_file, visualwakewords_annotations_train,
-      small_object_area_threshold, foreground_class_of_interest,
-      visualwakewords_labels_filename)
-  build_visualwakewords_data_lib.create_visual_wakeword_annotations(
-      FLAGS.val_annotations_file, visualwakewords_annotations_val,
-      small_object_area_threshold, foreground_class_of_interest,
-      visualwakewords_labels_filename)
+    # Path to COCO dataset images and annotations
+    assert FLAGS.train_image_dir, '`train_image_dir` missing.'
+    assert FLAGS.val_image_dir, '`val_image_dir` missing.'
+    assert FLAGS.train_annotations_file, '`train_annotations_file` missing.'
+    assert FLAGS.val_annotations_file, '`val_annotations_file` missing.'
+    visualwakewords_annotations_train = os.path.join(
+        FLAGS.output_dir, 'instances_visualwakewords_train2014.json')
+    visualwakewords_annotations_val = os.path.join(
+        FLAGS.output_dir, 'instances_visualwakewords_val2014.json')
+    visualwakewords_labels_filename = os.path.join(FLAGS.output_dir,
+                                                   'labels.txt')
+    small_object_area_threshold = FLAGS.small_object_area_threshold
+    foreground_class_of_interest = FLAGS.foreground_class_of_interest
+    # Create the Visual WakeWords annotations from COCO annotations
+    if not tf.io.gfile.isdir(FLAGS.output_dir):
+        tf.io.gfile.makedirs(FLAGS.output_dir)
+    build_visualwakewords_data_lib.create_visual_wakeword_annotations(
+        FLAGS.train_annotations_file, visualwakewords_annotations_train,
+        small_object_area_threshold, foreground_class_of_interest,
+        visualwakewords_labels_filename)
+    build_visualwakewords_data_lib.create_visual_wakeword_annotations(
+        FLAGS.val_annotations_file, visualwakewords_annotations_val,
+        small_object_area_threshold, foreground_class_of_interest,
+        visualwakewords_labels_filename)
 
-  # Create the TF Records for Visual WakeWords Dataset
-  if not tf.gfile.IsDirectory(FLAGS.output_dir):
-    tf.gfile.MakeDirs(FLAGS.output_dir)
-  train_output_path = os.path.join(FLAGS.output_dir, 'train.record')
-  val_output_path = os.path.join(FLAGS.output_dir, 'validation.record')
-  build_visualwakewords_data_lib.create_tf_record_for_visualwakewords_dataset(
-      visualwakewords_annotations_train,
-      FLAGS.train_image_dir,
-      train_output_path,
-      num_shards=100)
-  build_visualwakewords_data_lib.create_tf_record_for_visualwakewords_dataset(
-      visualwakewords_annotations_val,
-      FLAGS.val_image_dir,
-      val_output_path,
-      num_shards=10)
+    # Create the TF Records for Visual WakeWords Dataset
+    if not tf.io.gfile.isdir(FLAGS.output_dir):
+        tf.io.gfile.makedirs(FLAGS.output_dir)
+    train_output_path = os.path.join(FLAGS.output_dir, 'train.record')
+    val_output_path = os.path.join(FLAGS.output_dir, 'validation.record')
+    build_visualwakewords_data_lib.create_tf_record_for_visualwakewords_dataset(
+        visualwakewords_annotations_train,
+        FLAGS.train_image_dir,
+        train_output_path,
+        num_shards=100)
+    build_visualwakewords_data_lib.create_tf_record_for_visualwakewords_dataset(
+        visualwakewords_annotations_val,
+        FLAGS.val_image_dir,
+        val_output_path,
+        num_shards=10)
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.compat.v1.app.run()
